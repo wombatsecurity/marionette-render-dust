ec0284ab246e2685e1c853b7e44cd64b8ae66441
Jason Brubaker <jrrbrubaker@gmail.com>
2017-07-24 13:52:24 -0400

    Remove assumption of template wrapper (fix #4) (#5)
    * Remove assumption of template wrapper.
    
    Update package.json (major version)
    
    * Revert "let" to "var" for old IE browsers

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//for whatever reason, Backbone.$ wasn't getting set, so I set it manually
+// for whatever reason, Backbone.$ wasn't getting set, so I set it manually
 var $ = require( 'jquery' );
 var Backbone = require( 'backbone' );
 Backbone.$ = $;
@@ -17,13 +17,23 @@ var sinon = require( "sinon" );
 var sinonChai = require( "sinon-chai" );
 
 // set up test stuff
+dust.config.whitespace = true;
 chai.use( sinonChai );
 var expect = chai.expect;
+
+// load/require fixtures
+const dustfile = fs.readFileSync( path.join( __dirname, './fixtures/test.dust' ), 'utf8' ),
+	context = require( './fixtures/data.json' ),
+	output = fs.readFileSync( path.join( __dirname, './fixtures/output.html' ), 'utf8' );
+
+// compile & register template
+const compiled = dust.compile( dustfile, 'test' ),
+	template = dust.loadSource( compiled );
 
 // Define a function that we will use to set up the Renderer
 function setUpRendering( options ) {
 	// delete the cached version of the index page
-	delete require.cache[ require.resolve( '../index' ) ];
+	delete require.cache[require.resolve( '../index' )];
 
 	// require the main index file of the package
 	var renderer;
@@ -39,12 +49,12 @@ function setUpRendering( options ) {
 
 // set up some fixtures used in the tests
 var html = "<div>Some html</div>";
-var renderFn = function( data, callback ) {
+var renderFn = function ( data, callback ) {
 	callback( null, html );
 };
 
 // define our specification
-describe( 'marionette-dust-render', function() {
+describe( 'marionette-dust-render', function () {
 	var render, sandbox;
 	var outputDir = path.resolve( __dirname, './output' );
 	var outputFile = 'pack_to_test.js';
@@ -52,10 +62,10 @@ describe( 'marionette-dust-render', function() {
 
 
 	// Remove test file before each test
-	beforeEach( function( done ) {
+	beforeEach( function ( done ) {
 		sandbox = sinon.sandbox.create();
 
-		rimraf( outputPath, function( err ) {
+		rimraf( outputPath, function ( err ) {
 			if ( err ) {
 				return done( err );
 			} else {
@@ -64,13 +74,13 @@ describe( 'marionette-dust-render', function() {
 		} );
 	} );
 
-	afterEach( function() {
+	afterEach( function () {
 		sandbox.restore();
 	} );
 
 	// Remove output directory after all tests are done
-	after( function( done ) {
-		rimraf( outputDir, function( err ) {
+	after( function ( done ) {
+		rimraf( outputDir, function ( err ) {
 			if ( err ) {
 				return done( err );
 			} else {
@@ -79,209 +89,175 @@ describe( 'marionette-dust-render', function() {
 		} );
 	} );
 
-	describe( "Passing an incorrect template", function() {
-		before( function() {
+	describe( "Passing an incorrect template", function () {
+		before( function () {
 			render = setUpRendering();
 		} );
 
-		it( "like a null value should throw an error", function() {
-			expect( function() {
+		it( "like a null value should throw an error", function () {
+			expect( function () {
 				render( null, {} );
 			} ).to.throw( Marionette.Error );
 		} );
 
-		it( "like an empty string should throw an error", function() {
-			expect( function() {
+		it( "like an empty string should throw an error", function () {
+			expect( function () {
 				render( "", {} );
 			} ).to.throw( Marionette.Error );
 		} );
 
-		it( "like an undefined value should throw an error", function() {
-			expect( function() {
+		it( "like an undefined value should throw an error", function () {
+			expect( function () {
 				render();
 			} ).to.throw( Marionette.Error );
 		} );
 	} );
 
-	describe( "Passing a correct template", function() {
-		before( function() {
+	describe( "Passing a correct template", function () {
+		before( function () {
 			render = setUpRendering();
 		} );
 
-		describe( "as a function", function() {
-			it( "should not throw an error", function() {
-				expect( function() {
-					render( function() {}, {} );
+		describe( "as a function", function () {
+			it( "should not throw an error", function () {
+				expect( function () {
+					render( template, context );
 				} ).to.not.throw();
 			} );
 
-			it( "should call the function to render the template", function() {
-				var o = {
-					f: function() {}
-				};
-				sandbox.spy( o, "f" );
-				render( o.f, {} );
-				expect( o.f ).to.have.been.called;
-			} );
-
-			it( "should leave it up to the function on what to return", function() {
-				var ret = render( renderFn, {} );
-				expect( ret ).to.equal( html );
+			it( "should correctly render the template", function () {
+				var ret = render( template, context );
+				expect( ret ).to.equal( output );
 			} );
 		} );
 
-		describe( "as a string", function() {
-			beforeEach( function() {
-				sandbox.stub( dust, "render", function( template ) {
-					return template;
-				} );
-			} );
-
-			it( "should not throw an error", function() {
-				expect( function() {
-					render( "<div></div>", {} );
+		describe( "as a string", function () {
+			it( "should not throw an error", function () {
+				expect( function () {
+					render( "test", context );
 				} ).to.not.throw();
 			} );
 
-			it( "should call dust's render function", function() {
-				render( "<div></div>", {} );
-				expect( dust.render ).to.have.been.called;
+			it( "should correctly render the template", function () {
+				var ret = render( 'test', context );
+				expect( ret ).to.equal( output );
 			} );
 
 		} );
 	} );
 
 
-	describe( "Passing a post-processor function", function() {
+	describe( "Passing a post-processor function", function () {
 		var options = {
-				postProcessor: function( html ) {
-					return html + " and changes";
-				}
-			},
-			data = {
-				foo: 'bar'
-			},
+			postProcessor: function ( html ) {
+				return html + " and changes";
+			}
+		},
 			spy;
 
-		beforeEach( function() {
+		beforeEach( function () {
 			spy = sandbox.spy( options, 'postProcessor' );
 			render = setUpRendering( options );
 		} );
 
-		it( "should not throw an error", function() {
-			expect( function() {
-				render( renderFn, {} );
+		it( "should not throw an error", function () {
+			expect( function () {
+				render( template, context );
 			} ).to.not.throw();
 		} );
 
-		it( "should call the function with the appropriate parameters", function() {
-			var ret = render( renderFn, data );
-			expect( options.postProcessor ).to.have.been.calledWith( html, data );
+		it( "should call the function with the appropriate parameters", function () {
+			var ret = render( template, context );
+			expect( options.postProcessor ).to.have.been.calledWith( output, context );
 		} );
 
 		it(
 			"should allow you to alter what gets returned from the render function",
-			function() {
-				var ret = render( renderFn, data );
-				expect( ret ).not.to.equal( html );
-				expect( ret ).to.equal( html + " and changes" );
+			function () {
+				var ret = render( template, context );
+				expect( ret ).not.to.equal( output );
+				expect( ret ).to.equal( output + " and changes" );
 			} );
 	} );
 
-	describe( "Passing multiple post-processor functions", function() {
-		var options = {
-				postProcessors: [ function( html ) {
+	describe( "Passing multiple post-processor functions", function () {
+		beforeEach( function () {
+			render = setUpRendering( {
+				postProcessors: [function ( html ) {
 					return html + " and changes";
-				}, function( html, data ) {
-					return html + " " + data.foo;
-				} ]
-			},
-			data = {
-				foo: 'bar'
-			},
-			spy;
-
-		beforeEach( function() {
-			render = setUpRendering( options );
+				}, function ( html, data ) {
+					return html + " " + data.person;
+				}]
+			} );
 		} );
 
-		it( "should not throw an error", function() {
-			expect( function() {
-				render( renderFn, {} );
+		it( "should not throw an error", function () {
+			expect( function () {
+				render( template, context );
 			} ).to.not.throw();
 		} );
 
-		it( "should apply the post-processors in order", function() {
-			var ret = render( renderFn, data );
-			expect( ret ).not.to.equal( html );
-			expect( ret ).to.equal( html + " and changes bar" );
+		it( "should apply the post-processors in order", function () {
+			var ret = render( template, context );
+			expect( ret ).not.to.equal( output );
+			expect( ret ).to.equal( output + " and changes you" );
 		} );
 	} );
 
-	describe( "The renderer.addPostProcessor() method", function() {
-		var data = {
-			foo: 'bar'
-		};
-
-		var first = function( html, data ) {
-				return html + " first";
-			},
-			second = function( html, data ) {
+	describe( "The renderer.addPostProcessor() method", function () {
+		var first = function ( html, data ) {
+			return html + " first";
+		},
+			second = function ( html, data ) {
 				return html + " second";
 			},
-			foo = function( html, data ) {
-				return html + " " + data.foo;
+			foo = function ( html, data ) {
+				return html + " " + data.person;
 			};
 
-		beforeEach( function() {
+		beforeEach( function () {
 			render = setUpRendering();
 		} );
 
-		it( "should add post-processors in order", function() {
+		it( "should add post-processors in order", function () {
 			render.addPostProcessor( first );
-			expect( render( renderFn, data ) ).to.equal( html + " first" );
+			expect( render( template, context ) ).to.equal( output + " first" );
 			render.addPostProcessor( second );
-			expect( render( renderFn, data ) ).to.equal( html + " first second" );
+			expect( render( template, context ) ).to.equal( output + " first second" );
 		} );
 
 
 		it(
 			"should add a post-processor in the right position if index is provided",
-			function() {
+			function () {
 				render.addPostProcessor( first );
 				render.addPostProcessor( foo );
 				render.addPostProcessor( second, 1 );
 
-				expect( render( renderFn, data ) ).to.equal( html +
-					" first second bar" );
+				expect( render( template, context ) ).to.equal( output +
+					" first second you" );
 			} );
 	} );
 
-	describe( "the renderer.removePostProcessor() method", function() {
-		var one = function( html ) {
-				return html + " and changes";
-			},
-			two = function( html, data ) {
-				return html + " " + data.foo;
+	describe( "the renderer.removePostProcessor() method", function () {
+		var one = function ( html ) {
+			return html + " and changes";
+		},
+			two = function ( html, data ) {
+				return html + " " + data.person;
 			}
 
-		var options = {
-				postProcessors: [ one, two ]
-			},
-			data = {
-				foo: 'bar'
-			},
-			spy;
-
-		beforeEach( function() {
-			render = setUpRendering( options );
+		beforeEach( function () {
+			render = setUpRendering( {
+				postProcessors: [one, two]
+			} );
 		} );
 
-		it( "removes the specified post-processor", function() {
+		it( "removes the specified post-processor", function () {
 			render.removePostProcessor( one );
-			expect( render( renderFn, data ) ).to.equal( html + " bar" );
+			expect( render( template, context ) ).to.equal( output + " you" );
 			render.removePostProcessor( two );
-			expect( render( renderFn, data ) ).to.equal( html );
+			expect( render( template, context ) ).to.equal( output );
 		} );
 
 	} );
